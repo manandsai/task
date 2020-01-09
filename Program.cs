b7d870aca453ff50d22e253de701fb77e291d83a
manandsai <57131751+manandsai@users.noreply.github.com>
2020-01-09 14:13:48 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task
+{
+    [Serializable ]    
+     public class Program
+    {
+        static void Main(string[] args)
+        {
+              Menu menu = new Menu();
+              menu.MenuList();
+        }
+    }
+}
